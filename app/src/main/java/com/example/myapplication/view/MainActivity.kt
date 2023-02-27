2bb6cefb7b8fad221dd6d4f7d21c98489834585d
Mahendra <mahendrasutar8.ms@gmail.com>
2023-02-27 21:04:34 +0530

    completed

--- app/src/main/java/com/example/myapplication/view/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/view/MainActivity.kt
@@ -3,20 +3,30 @@ package com.example.myapplication.view
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.activity.viewModels
+import com.bumptech.glide.Glide
 import com.example.myapplication.R
+import com.example.myapplication.databinding.ActivityMainBinding
 import com.example.myapplication.viewmodel.ViewModel
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
-
+    lateinit var binding : ActivityMainBinding
     private val viewModel by viewModels<ViewModel>()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
         viewModel.authorData.observe(this){
+            if(!it.isNullOrEmpty()){
+                binding.authorName.text = it[0].author
+                Glide.with(this).load(it[0].download_url).into(binding.authorImg)
+            }
+        }
 
+        binding.showBtn.setOnClickListener {
+            viewModel.getAuthorData()
         }
     }
 }
